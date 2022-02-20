94e0c90d88cf8f24aa17b6264bac57b004a28f94
Gábor Koleszár <gabor.koleszar@protonail.ch>
2022-02-20 19:34:13 +0100

    Change theme

--- app/src/main/java/com/example/cryptolist/MainActivity.kt
+++ app/src/main/java/com/example/cryptolist/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.cryptolist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cryptolist.ui.theme.CryptoListTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +12,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoListTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
 
             }
         }
